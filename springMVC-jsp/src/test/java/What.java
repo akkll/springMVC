d04e0d5af74408ab1649a82e16caf0d5c01673d9
akkll <77881154+akkll@users.noreply.github.com>
2022-01-27 19:38:34 +0800

    Update What.java

--- springMVC-jsp/src/test/java/What.java
+++ springMVC-jsp/src/test/java/What.java
@@ -12,5 +12,6 @@ public class What {
         System.out.println("Hello,Git4! by hot-fix");
         System.out.println("Hello,Git5! by now");
         System.out.println("Hello,Git6! by master");
+        System.out.println("Hello,Git7! by Github");
     }
 }
